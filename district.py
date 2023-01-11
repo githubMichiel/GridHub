3e4cbcda7ca3ac3543e5e727f6834cf6d2d5b722
skip-vial <skipvial@gmail.com>
2023-01-11 15:28:08 +0100

    finalized load functions

--- district.py
+++ district.py
@@ -14,12 +14,12 @@ class District:
         """ load batteries into memory"""
 
         with open(filename, "r") as f:
-
             csvreader = csv.reader(f)
+
+            # skip first line
             next(csvreader)
 
             for row in csvreader:
-
                 x = row[0]
                 y = row[1]
                 capacity = row[2].strip()
@@ -34,24 +34,19 @@ class District:
 
         with open(filename) as f:
             csvreader = csv.reader(f)
+
+            # skip first line
             next(csvreader)
 
             for row in csvreader:
-                # read each line separately
-                line = f.readline()
-
-                # split line into components
-                line = line.split(",")
-                x = line[0]
-                y = line[1]
-                max_output = line[2].strip()
+                x = row[0]
+                y = row[1]
+                max_output = row[2].strip()
 
                 # create house
                 house = House(x, y, max_output)
                 self.houses.append(house)
 
-                if line == "\n":
-                    break
 #    def __str__(self):
 
 #        return f'[\n\{"district: "}]'
