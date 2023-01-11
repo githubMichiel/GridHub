afdbd565dc5742cd9db7111b02e6a95226fe360b
reinp22 <rgmpostma@gmail.com>
2023-01-11 11:52:02 +0100

    create class House

---
+++ loader.py
@@ -0,0 +1,14 @@
+# create a class that describes a 'house' object
+
+class House():
+
+    def __init__(self, x, y, maxoutput):
+        self.x = x_axis
+        self.y = y_axis
+        self.maxoutput = maxoutput
+
+        # location of the house
+        self.location = f"{x_axis}, {y_axis}"
+
+        # make list with corresponding cable location
+        self.cables = []
