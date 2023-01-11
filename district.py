205d8f5104a3bf8bc8436ce1477a40dcec9eff9e
Michiel <m.michiel.bos@gmail.com>
2023-01-11 12:04:11 +0100

    district

---
+++ district.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class District:
+    def __init__(self):
+        self.batteries = []
+        self.houses = []
+
+
+    def add_house(self, house):
+        self.houses.append(house)
+
+    def add_battery(self, battery):
+        self.batteries.append(battery)
+
+#    def __str__(self):
+
+#        return f'[\n\{"district: "}]'
