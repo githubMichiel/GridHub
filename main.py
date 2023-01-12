237d42d8ed3a10422b30c562f910a0e50dc405c4
skip-vial <skipvial@gmail.com>
2023-01-12 11:24:12 +0100

    manually added cable representation

--- main.py
+++ main.py
@@ -12,17 +12,8 @@ from battery import Battery
 from house import House
 from district import District
 
-<<<<<<< HEAD
-def json_format(district):
-    district_info = repr(district)
-    #TODO: find a way to do this for all batteries without hard coding it
-    batteries = [repr(district.batteries[0]),repr(district.batteries[1]),repr(district.batteries[2]),repr(district.batteries[3]),repr(district.batteries[4])]
-    return f'[{{{district_info}}}, {{{batteries[0]}}}, {{{batteries[1]}}}, {{{batteries[2]}}}, {{{batteries[3]}}}, {{{batteries[4]}}}]'
-
-def json_output(string):
-    parsed = json.loads(string)
-    print(json.dumps(parsed,indent=2))
-
+def json_output():
+    return
 
 if __name__ == "__main__":
 
@@ -37,9 +28,11 @@ if __name__ == "__main__":
     districts[2].load_houses('district-3_houses.csv')
     districts[2].load_batteries('district-3_batteries.csv')
 
-    test = json_format(districts[0])
-    json_output(test)
-
+    #print(district[0].houses)
+    #print(district[0].batteries)
+    #print(json_output())
+    #parsed = json.loads(json_output())
+    #print(json.dumps(parsed, indent=4))
 
     # lists with x, y coordinates per district
     battery_1_x = []
@@ -73,22 +66,27 @@ if __name__ == "__main__":
         house_3_x.append(int(districts[2].houses[i].x))
         house_3_y.append(int(districts[2].houses[i].y))
 
+
     # figure district 1
     fig1 = plt.figure()
     ax1 = fig1.add_subplot()
 
     ax1.scatter(battery_1_x, battery_1_y, s=80, c='r', marker="P", label='first')
-    ax1.scatter(house_1_x,house_1_y, s=80, c='b', marker="p", label='second')
+    ax1.scatter(house_1_x, house_1_y, s=80, c='b', marker="p", label='second')
     plt.xticks(np.arange(0, 51, step=1))
     plt.yticks(np.arange(0, 51, step=1))
     plt.grid(linestyle='--', linewidth=0.5)
+
+    x1 = np.array([38, 39, 39, 39])
+    y1 = np.array([12, 12, 13, 14])
+    plt.plot(x1, y1, c='black')
 
     # figure district 2
     fig2 = plt.figure()
     ax2 = fig2.add_subplot()
 
     ax2.scatter(battery_2_x, battery_2_y, s=80, c='r', marker="P", label='first')
-    ax2.scatter(house_2_x,house_2_y, s=80, c='b', marker="p", label='second')
+    ax2.scatter(house_2_x, house_2_y, s=80, c='b', marker="p", label='second')
     plt.xticks(np.arange(0, 51, step=1))
     plt.yticks(np.arange(0, 51, step=1))
     plt.grid(linestyle='--', linewidth=0.5)
@@ -98,7 +96,7 @@ if __name__ == "__main__":
     ax3 = fig3.add_subplot()
 
     ax3.scatter(battery_3_x, battery_3_y, s=80, c='r', marker="P", label='first')
-    ax3.scatter(house_3_x,house_3_y, s=80, c='b', marker="p", label='second')
+    ax3.scatter(house_3_x, house_3_y, s=80, c='b', marker="p", label='second')
     plt.xticks(np.arange(0, 51, step=1))
     plt.yticks(np.arange(0, 51, step=1))
     plt.grid(linestyle='--', linewidth=0.5)
