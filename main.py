a2195abec734f8a7a98a6019a93ecf7d94a357c8
skip-vial <skipvial@gmail.com>
2023-01-16 12:14:12 +0100

    added plot function

--- main.py
+++ main.py
@@ -22,6 +22,23 @@ from district import District
     #parsed = json.loads(string)
     #print(json.dumps(parsed,indent=2))
 
+def plot_district(district):
+    """ create a visualization of a district"""
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    ax.scatter([battery_1.x for battery_1 in districts[district].batteries], [battery_1.y for battery_1 in districts[district].batteries], s=80, c='r', marker="P", label='batteries')
+    ax.scatter([house_1.x for house_1 in districts[district].houses],[house_1.y for house_1 in districts[district].houses], s=80, c='b', marker="p", label='houses')
+    plt.xticks(np.arange(0, 51, step=1))
+    plt.yticks(np.arange(0, 51, step=1))
+    plt.grid(linestyle='--', linewidth=0.5)
+
+    # manually plot cables
+    x1 = np.array([38, 39, 39, 39])
+    y1 = np.array([12, 12, 13, 14])
+    plt.plot(x1, y1, c='black')
+
 
 if __name__ == "__main__":
 
@@ -41,45 +58,14 @@ if __name__ == "__main__":
     #test = json_format(districts[0])
     #json_output(test)
 
-    # figure district 1
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot()
-
-    ax1.scatter([battery_1.x for battery_1 in districts[0].batteries], [battery_1.y for battery_1 in districts[0].batteries], s=80, c='r', marker="P", label='first')
-    ax1.scatter([house_1.x for house_1 in districts[0].houses],[house_1.y for house_1 in districts[0].houses], s=80, c='b', marker="p", label='second')
-    plt.xticks(np.arange(0, 51, step=1))
-    plt.yticks(np.arange(0, 51, step=1))
-    plt.grid(linestyle='--', linewidth=0.5)
-
-    # manually plot cables
-    x1 = np.array([38, 39, 39, 39])
-    y1 = np.array([12, 12, 13, 14])
-    plt.plot(x1, y1, c='black')
-
-    # figure district 2
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot()
-
-    ax2.scatter([battery_2.x for battery_2 in districts[1].batteries], [battery_2.y for battery_2 in districts[1].batteries], s=80, c='r', marker="P", label='first')
-    ax2.scatter([house_2.x for house_2 in districts[1].houses], [house_2.y for house_2 in districts[1].houses], s=80, c='b', marker="p", label='second')
-    plt.xticks(np.arange(0, 51, step=1))
-    plt.yticks(np.arange(0, 51, step=1))
-    plt.grid(linestyle='--', linewidth=0.5)
-
-    # figure district 3
-    fig3 = plt.figure()
-    ax3 = fig3.add_subplot()
-
-    ax3.scatter([battery_3.x for battery_3 in districts[2].batteries], [battery_3.y for battery_3 in districts[2].batteries], s=80, c='r', marker="P", label='first')
-    ax3.scatter([house_3.x for house_3 in districts[2].houses], [house_3.y for house_3 in districts[2].houses], s=80, c='b', marker="p", label='second')
-    plt.xticks(np.arange(0, 51, step=1))
-    plt.yticks(np.arange(0, 51, step=1))
-    plt.grid(linestyle='--', linewidth=0.5)
-
+    # visualize each district
+    plot_district(0)
+    plot_district(1)
+    plot_district(2)
     plt.show()
 
-    # connect houses to 1_batteries
-    districts[0].connect_house_battery()
-    print(districts[0].houses[0].battery.id)
-
-    districts[0].list_houses_battery()
+    # connect each house to a battery
+    for district in districts:
+        district.connect_house_battery()
+        print(district.houses[0].battery.id)
+        district.list_houses_battery()
